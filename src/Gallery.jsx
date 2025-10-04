b91aef21e213f81a703e8624d42ded9c91b4418a
Orlando G.V <orlando.gal.var05@gmail.com>
2025-10-03 18:24:26 -0600

    feat: add Gallery component with Profile subcomponent

---
+++ src/Gallery.jsx
@@ -0,0 +1,18 @@
+export function Profile() {
+     return (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/6/60/Doorss_electra_publicity_photo.JPG"
+      alt="Image The Doors"
+      />
+  );
+}
+
+export default function Gallery() {
+   return (
+    <>
+     <Profile />
+     <Profile />
+     <Profile />
+     </>
+   )
+}
